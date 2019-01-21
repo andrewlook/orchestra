97c29a2b8912d90064230c71f98ff0f2d3eabf27
Andrew Look <andrew.m.look@gmail.com>
2019-01-21 16:32:19 -0500

    comment out task notification since email doesnt seem to work.

--- orchestra/utils/task_lifecycle.py
+++ orchestra/utils/task_lifecycle.py
@@ -251,7 +251,7 @@ def assign_task(worker_id, task_id, staffing_request_inquiry=None):
 
     if settings.PRODUCTION or settings.STAGING:
         add_worker_to_project_team(worker, task.project)
-    notify_status_change(task, previous_status)
+    # notify_status_change(task, previous_status)
     mark_worker_as_winner(worker, task, required_role_counter,
                           staffing_request_inquiry)
     return task
